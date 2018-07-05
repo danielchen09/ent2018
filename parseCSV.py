23d6866db100627dabf8af51267d0c8a917c9c71
winnieli1129 <winnieli@pacificamerican.org>
2018-07-05 16:40:48 +0800

    Update parseCSV.py

--- parseCSV.py
+++ parseCSV.py
@@ -5,9 +5,7 @@ import pymysql.cursors
 connection = pymysql.connect(host = "localhost",
                              user = "root",
                              password = "101dc101",
-                             db = "hospitalApp",
-                             charset = "utf8mb4",
-                             cursorclass = pymysql.cursors.DictCursor)
+                             db = "hospitalApp")
 
 count = 0;
 
@@ -16,9 +14,11 @@ with open("TaiwanHospitalInformation.csv", encoding = "utf-8") as file:
     names = []
     addr = []
     phone = []
+    type = []
     for row in reader:
         names.append(row[1])
         addr.append(row[6])
+        type.append(row[3])
         if count == 0:
             count += 1
             phoneNum = row[5]
@@ -35,16 +35,19 @@ with open("TaiwanHospitalInformation.csv", encoding = "utf-8") as file:
     names.remove("機構名稱")
     addr.remove("地址")
     phone.remove("電話")
-    for x, y, z in zip(names, addr, phone):
-        print(x)
-        print(y)
-        print(z)
-        print()
+    type.remove("型態別")
+    for x, y, z, a in zip(names, addr, phone, type):
+
+        if(a == "綜合醫院"):
+            print(x)
+            print(y)
+            print(z)
+            print()
 
 try:
     with connection.cursor() as cursor:
-        sql = "INSERT INTO `hositalInfo` (`name`, `address`, `phone number`) VALUES (%s, %s, %s)"
-        for x, y, z in zip(names, addr, phone):
+        sql = "INSERT INTO `HOSPITALINFO` (`name`, `address`, `phone number`, `type1) VALUES (%s, %s, %s, %s)"
+        for x, y, z, a in zip(names, addr, phone, type):
             cursor.execute(sql, (x,y,z))
 
     connection.commit()
